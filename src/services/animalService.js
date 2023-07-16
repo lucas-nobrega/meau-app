9c6df3b1636d87cd534afa10df9841c0dba270ae
Gustavo Rodrigues da Costa <31167914+gusrodrigues@users.noreply.github.com>
2023-07-16 16:30:48 -0300

    Update animalService.js
    Adicionar função que busca meus animais e função que busca animais para adoção

--- src/services/animalService.js
+++ src/services/animalService.js
@@ -10,7 +10,22 @@ const animalService = {
         return animals;
     },
     //PEGAR ANIMAIS QUE PODER SER ADOTADOS E QUE NÃO SÃO DO USUÁRIO
+    // Documentação: https://firebase.google.com/docs/firestore/query-data/queries?hl=pt&authuser=0
+    async getAnimalsAdoption(userId) {
+        const q = query(collection(db, "animais"), where("paraAdocao", "==", true), where("respoensavelId", "!=", userId));
+        const querySnapshot = await getDocs(q);
+        const animalsAdoption = querySnapshot.docs.map(doc => doc.data());
+        console.log("Animais para adoção:", animalsAdoption);
+        return animalsAdoption;
+    },
     //PEGAR ANIMAIS QUE TENHAM O MEU RESPONSAVELID
+    async getMyAnimals(userId) {
+        const q = query(collection(db, "animais"), where("respoensavelId", "==", userId));
+        const querySnapshot = await getDocs(q);
+        const myAnimals = querySnapshot.docs.map(doc => doc.data());
+        console.log("Meus animais:", myAnimals);
+        return myAnimals;
+    },
     async createAnimal(animal) {
         const docRef = await addDoc(collection(db, "animais"), animal);
         console.log("Document written with ID: ", docRef.id);
